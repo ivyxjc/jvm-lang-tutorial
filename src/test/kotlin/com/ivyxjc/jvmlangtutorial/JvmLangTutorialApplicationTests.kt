20a7e812a7b7bbb5a71522bc46ec22793f67f231
JunchengXue <juncheng@magicloud.io>
2022-11-15 17:41:23 +0800

    :tada:

---
+++ src/test/kotlin/com/ivyxjc/jvmlangtutorial/JvmLangTutorialApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.ivyxjc.jvmlangtutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JvmLangTutorialApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+}
